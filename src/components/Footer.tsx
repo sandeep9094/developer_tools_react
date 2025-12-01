6c046ab383c7c20c09306b4a325d0c615e1b3bd1
cooperRooster <168619211+cooperRooster@users.noreply.github.com>
2025-12-01 14:57:28 +0530

    updated

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,14 +3,14 @@ import './Footer.css'
 function Footer() {
   const currentYear = new Date().getFullYear()
 
-  return (
-    <footer className="app-footer">
-      <div className="footer-content">
-        <p>&copy; {currentYear} Developed by Team Tri Nova - SRK.</p>
-      </div>
-      <p>Sandeep, Rajat, Kanwar</p>
-    </footer>
-  )
+return (
+  <footer className="app-footer">
+    <div className="footer-content">
+      <p>&copy; {currentYear} Developed by Team Tri Nova - SRK.</p>
+      <p>Sandeep Rajat Kanwar</p>
+    </div>
+  </footer>
+)
 }
 
 export default Footer
